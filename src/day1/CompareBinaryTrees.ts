b27bda1e0c83a0f63daa7c528b00ecaae1472536
David Lascurain <68433067+LockheadDev@users.noreply.github.com>
2024-01-28 10:17:21 -0600

    Class 28-jan-2024

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    //structural check
+    if (a == null && b == null) {
+        return true;
+    }
 
+    //structural check
+    if (a == null || b == null) {// the correct way to catch this should be using an XOR but that case is taken by the AND above
+        return false;
+    }
+
+    //value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
